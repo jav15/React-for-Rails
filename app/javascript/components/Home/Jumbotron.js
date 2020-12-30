c8bd254cc3674cc3294083bc381ced3fa480cb33
Jav <iglesiasjavier15@gmail.com>
2020-12-30 18:34:59 -0500

    v0.1

---
+++ app/javascript/components/Home/Jumbotron.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react'
+
+class Jumbotron = () => {
+return (
+	<section className="home-section--1">
+		<div className="container">
+			<div className="row"> 
+				<div className="col col-sm-12 col-md-5">
+					<div className="pt-4 mt-4">
+						<h1>React For Rails Developers</h1>
+						<p>Supercharge your Ruby on Rails App with React.js</p>
+					</div>
+				</div>
+				<div className="col col-sm-12 col-md-7"></div>
+			</div>
+		</div>
+	</section>
+	)
+}
+
+export default Jumbotron
